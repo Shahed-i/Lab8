8ed54ff96c48571f0a290d0f4cae80656938aa24
Shahed-i <shahedissa9@gmail.com>
2024-03-11 13:02:35 -0600

    fixed names

--- products-app/src/components/HomePage.js
+++ products-app/src/components/HomePage.js
@@ -38,11 +38,11 @@ const HomePage = () => {
                 </thead>
                 <tbody>
                     <tr>
-                        <td style={styles.td}>A B</td>
+                        <td style={styles.td}>Shahed Issa</td>
                         <td style={styles.td}>Developer</td>
                     </tr>
                     <tr>
-                        <td style={styles.td}>C D</td>
+                        <td style={styles.td}>Neha Parmar</td>
                         <td style={styles.td}>Designer</td>
                     </tr>
                 </tbody>
